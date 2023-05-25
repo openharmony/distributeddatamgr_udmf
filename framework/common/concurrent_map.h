cc604cccf57b24de68956615ffed20f75dff1b25
e <wangkai424@huawei.com>
2023-05-25 18:48:41 +0800

    change1
    Signed-off-by: e <wangkai424@huawei.com>

--- framework/common/concurrent_map.h
+++ framework/common/concurrent_map.h
@@ -17,6 +17,7 @@
 #define OHOS_UDMF_FRAMEWORKS_COMMON_CONCURRENT_MAP_H
 #include <functional>
 #include <map>
+#include <mutex>
 namespace OHOS {
 template<typename _Key, typename _Tp>
 class ConcurrentMap {
