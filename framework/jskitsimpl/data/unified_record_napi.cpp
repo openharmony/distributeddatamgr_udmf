5108c689ba9106bbcbb4492815f24bfb645a92c1
zph <zhaopenghui5@huawei.com>
2024-04-13 18:31:36 +0800

    update
    Signed-off-by: zph <zhaopenghui5@huawei.com>

--- framework/jskitsimpl/data/unified_record_napi.cpp
+++ framework/jskitsimpl/data/unified_record_napi.cpp
@@ -12,8 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <cstdint>
-#include <vector>
 #define LOG_TAG "UnifiedRecordNapi"
 #include "unified_record_napi.h"
 
