daf89836191992de0875184e0c71ecc6819f7dba
wanxiaoqing <wanxiaoqing@huawei.com>
2024-09-04 18:48:35 +0800

    add test case
    Signed-off-by: wanxiaoqing <wanxiaoqing@huawei.com>

---
+++ framework/common/unittest/mock/tlv_util_mock.cpp
@@ -0,0 +1,435 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "tlv_util.h"
+
+#include "logger.h"
+
+namespace OHOS {
+namespace TLVUtil {
+template<>
+bool CountBufferSize(const std::shared_ptr<UnifiedRecord> &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool CountBufferSize(const Runtime &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool CountBufferSize(const UnifiedData &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool CountBufferSize(const std::vector<UnifiedData> &input, TLVObject &data)
+{
+    return true;
+}
+
+template<typename T>
+bool Writing(const T &input, TLVObject &data);
+
+template<typename T>
+bool Reading(T &output, TLVObject &data);
+
+template<>
+bool Writing(const int32_t &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(int32_t &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const int64_t &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(int64_t &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(uint32_t &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const uint32_t &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(uint64_t &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const uint64_t &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const bool &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(bool &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const std::string &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(std::string &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const std::vector<uint8_t> &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(std::vector<uint8_t> &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const UDVariant &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(UDVariant &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const ValueType &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(ValueType &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const UDDetails &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(UDDetails &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const UDType &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(UDType &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const Text &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(Text &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const PlainText &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(PlainText &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const Html &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(Html &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const Link &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(Link &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const File &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(File &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const Image &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(Image &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const Video &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(Video &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const Audio &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(Audio &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const Folder &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(Folder &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const SystemDefinedRecord &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(SystemDefinedRecord &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const SystemDefinedForm &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(SystemDefinedForm &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const SystemDefinedAppItem &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(SystemDefinedAppItem &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const SystemDefinedPixelMap &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(SystemDefinedPixelMap &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const ApplicationDefinedRecord &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(ApplicationDefinedRecord &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const std::shared_ptr<UnifiedRecord> &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(std::shared_ptr<UnifiedRecord> &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const UnifiedData &input, TLVObject &data)
+{
+    return false;
+}
+
+template<>
+bool Reading(UnifiedData &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const std::vector<UnifiedData> &input, TLVObject &data)
+{
+    return false;
+}
+
+template<>
+bool Reading(std::vector<UnifiedData> &output, TLVObject &data)
+{
+    return false;
+}
+
+template<>
+bool Writing(const UnifiedKey &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(UnifiedKey &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const Privilege &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(Privilege &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const DataStatus &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(DataStatus &output, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Writing(const Runtime &input, TLVObject &data)
+{
+    return true;
+}
+
+template<>
+bool Reading(Runtime &output, TLVObject &data)
+{
+    return true;
+}
+} // namespace TLVUtil
+} // namespace OHOS
