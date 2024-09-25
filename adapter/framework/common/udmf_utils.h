a29cf2455d0675474d619e294486db9447702f09
quguiren <quguiren@huawei.com>
2024-09-25 13:56:22 +0800

    cherry pick e5eaa6c from https://gitee.com/qugrsoft/distributeddatamgr_udmf/pulls/349 fix crossPlatformcompile
    Signed-off-by: quguiren <quguiren@huawei.com>

---
+++ adapter/framework/common/udmf_utils.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef UDMF_UTILS_H
+#define UDMF_UTILS_H
+
+#include <string>
+#include <vector>
+namespace OHOS {
+namespace UDMF {
+namespace UTILS {
+std::vector<std::string> StrSplit(const std::string &str, const std::string &delimiter);
+std::vector<uint8_t> Random(int32_t len, int32_t minimum = 0,
+                            int32_t maximum = std::numeric_limits<uint8_t>::max());
+std::string GenerateId();
+
+} // namespace UTILS
+} // namespace UDMF
+} // namespace OHOS
+
+#endif /* UDMF_UTILS_H */
