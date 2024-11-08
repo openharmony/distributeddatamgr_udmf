ceaff8a1e6f9c43a0c0f80d2a481545e54ff6a40
zph000 <zhaopenghui5@huawei.com>
2024-11-08 16:22:21 +0800

    update
    Signed-off-by: zph000 <zhaopenghui5@huawei.com>

--- framework/innerkitsimpl/data/preset_type_descriptors.cpp
+++ framework/innerkitsimpl/data/preset_type_descriptors.cpp
@@ -3287,6 +3287,20 @@ void PresetTypeDescriptors::InitDescriptors()
          {"audio/ac3"},
          "Audio Codec 3 File Format",
          REFERENCE_URL,
+         ""},
+        {"openharmony.hsp",
+         {"openharmony.package"},
+         {".hsp"},
+         {},
+         "Harmony Shared Package",
+         REFERENCE_URL,
+         ""},
+        {"openharmony.har",
+         {"openharmony.package"},
+         {".har"},
+         {},
+         "Harmony Archive",
+         REFERENCE_URL,
          ""}
     };
 };
--- interfaces/innerkits/common/unified_meta.h
+++ interfaces/innerkits/common/unified_meta.h
@@ -527,6 +527,8 @@ enum UDType : int32_t {
     CONTENT_FORM,
     M4P_AUDIO,
     AC3_AUDIO,
+    OPENHARMONY_HSP,
+    OPENHARMONY_HAR,
     UD_BUTT
 };
 
