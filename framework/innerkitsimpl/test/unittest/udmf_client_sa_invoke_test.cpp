80fe177210d07bbdcdf957cb372a9701c5ea43ec
lizhuojun <lizhuojun3@huawei.com>
2024-12-20 17:14:13 +0800

    udmf record limit 16M
    Signed-off-by: lizhuojun <lizhuojun3@huawei.com>

--- framework/innerkitsimpl/test/unittest/udmf_client_sa_invoke_test.cpp
+++ framework/innerkitsimpl/test/unittest/udmf_client_sa_invoke_test.cpp
@@ -264,18 +264,17 @@ HWTEST_F(UdmfClientSaInvokeTest, SetData006, TestSize.Level1)
     ASSERT_EQ(E_OK, status);
     ASSERT_EQ(2, readData.GetRecords().size());
     auto readPixelMap = std::static_pointer_cast<SystemDefinedPixelMap>(readData.GetRecordAt(0));
-    EXPECT_TRUE(readPixelMap->GetRawData().size() == 5 * 1024 * 1024
-        || readPixelMap->GetRawData().size() == 10 * 1024 * 1024);
+    EXPECT_EQ(5 * 1024 * 1024, readPixelMap->GetRawData().size());
 
     auto readPixelMap2 = std::static_pointer_cast<SystemDefinedPixelMap>(readData.GetRecordAt(1));
-    EXPECT_TRUE(readPixelMap2->GetRawData().size() == 5 * 1024 * 1024
-        || readPixelMap2->GetRawData().size() == 10 * 1024 * 1024);
+    EXPECT_TRUE(readPixelMap2->GetRawData().size() == 10 * 1024 * 1024);
+    EXPECT_EQ(10 * 1024 * 1024, readPixelMap2->GetRawData().size());
     LOG_INFO(UDMF_TEST, "SetData006 end.");
 }
 
 /**
 * @tc.name: SetData007
-* @tc.desc: Test set data error. Record size equals 4M.
+* @tc.desc: Test set data error. Record size Greater than 15.5M.
 * @tc.type: FUNC
 */
 HWTEST_F(UdmfClientSaInvokeTest, SetData007, TestSize.Level1)
@@ -284,7 +283,7 @@ HWTEST_F(UdmfClientSaInvokeTest, SetData007, TestSize.Level1)
 
     CustomOption option = { .intention = Intention::UD_INTENTION_DRAG };
     std::vector<uint8_t> rawData;
-    for (int i = 0; i < 128 * 1024 * 1024 + 1; i++) {
+    for (int i = 0; i < 15.5 * 1024 * 1024 + 1; i++) {
         rawData.emplace_back(1);
     }
     auto pixelMap = std::make_shared<SystemDefinedPixelMap>(rawData);
@@ -294,6 +293,48 @@ HWTEST_F(UdmfClientSaInvokeTest, SetData007, TestSize.Level1)
     auto status = UdmfClient::GetInstance().SetData(option, data, key);
     ASSERT_EQ(E_INVALID_PARAMETERS, status);
     LOG_INFO(UDMF_TEST, "SetData007 end.");
+}
+
+/**
+* @tc.name: SetData008
+* @tc.desc: Test set data error. Record size equals 15.5M.
+* @tc.type: FUNC
+*/
+HWTEST_F(UdmfClientSaInvokeTest, SetData008, TestSize.Level1)
+{
+    LOG_INFO(UDMF_TEST, "SetData008 begin.");
+
+    CustomOption option = { .intention = Intention::UD_INTENTION_DRAG };
+    std::vector<uint8_t> rawData;
+    for (int i = 0; i < 15.5 * 1024 * 1024; i++) {
+        rawData.emplace_back(1);
+    }
+    auto pixelMap = std::make_shared<SystemDefinedPixelMap>(rawData);
+    UnifiedData data;
+    data.AddRecord(pixelMap);
+
+    std::vector<uint8_t> rawData2;
+    for (int i = 0; i < 15 * 1024 * 1024; i++) {
+        rawData2.emplace_back(1);
+    }
+    auto pixelMap2 = std::make_shared<SystemDefinedPixelMap>(rawData2);
+    data.AddRecord(pixelMap2);
+
+    std::string key;
+    auto status = UdmfClient::GetInstance().SetData(option, data, key);
+    ASSERT_EQ(E_OK, status);
+
+    UnifiedData readData;
+    QueryOption queryOption = {.key = key};
+    status = UdmfClient::GetInstance().GetData(queryOption, readData);
+    ASSERT_EQ(E_OK, status);
+    ASSERT_EQ(2, readData.GetRecords().size());
+    auto readPixelMap1 = std::static_pointer_cast<SystemDefinedPixelMap>(readData.GetRecordAt(0));
+    EXPECT_EQ(15.5 * 1024 * 1024, readPixelMap1->GetRawData().size());
+
+    auto readPixelMap2 = std::static_pointer_cast<SystemDefinedPixelMap>(readData.GetRecordAt(1));
+    EXPECT_EQ(15 * 1024 * 1024, readPixelMap2->GetRawData().size());
+    LOG_INFO(UDMF_TEST, "SetData008 end.");
 }
 
 } // OHOS::Test
