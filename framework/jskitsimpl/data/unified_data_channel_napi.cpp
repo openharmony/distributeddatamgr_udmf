fd11caeab06dd78fcff7e988cab1f9816ffb3ed6
wanxiaoqing <wanxiaoqing@huawei.com>
2025-01-27 14:46:07 +0800

    key不存在抛出的异常code和message为空
    Signed-off-by: wanxiaoqing <wanxiaoqing@huawei.com>

--- framework/jskitsimpl/data/unified_data_channel_napi.cpp
+++ framework/jskitsimpl/data/unified_data_channel_napi.cpp
@@ -137,8 +137,10 @@ napi_value UnifiedDataChannelNapi::UpdateData(napi_env env, napi_callback_info i
     auto execute = [ctxt]() {
         QueryOption option = { .key = ctxt->key };
         auto status = UdmfClient::GetInstance().UpdateData(option, *(ctxt->unifiedData));
-        ASSERT_WITH_ERRCODE(ctxt, status == E_OK, E_INVALID_PARAMETERS,
-            "Parameter error: The unifiedData parameter is invalid!");
+        if (status == E_INVALID_PARAMETERS) {
+            ASSERT_WITH_ERRCODE(ctxt, status == E_OK, E_INVALID_PARAMETERS,
+                "Parameter error: The unifiedData parameter is invalid!");
+        }
     };
     return NapiQueue::AsyncWork(env, ctxt, std::string(__FUNCTION__), execute);
 }
@@ -181,8 +183,6 @@ napi_value UnifiedDataChannelNapi::QueryData(napi_env env, napi_callback_info in
         ASSERT_WITH_ERRCODE(ctxt, status == E_OK, status, "QueryData failed!");
     };
     auto output = [env, ctxt](napi_value &result) {
-        ASSERT_WITH_ERRCODE(ctxt, !ctxt->unifiedDataSet.empty(), E_INVALID_PARAMETERS,
-            "Parameter error: The unifiedDataSet parameter is empty!");
         ctxt->status = ConvertUnifiedDataSetToNapi(env, ctxt->unifiedDataSet, result);
         ASSERT_WITH_ERRCODE(ctxt, ctxt->status == napi_ok, E_ERROR, "ConvertUnifiedDataSetToNapi failed!");
     };
