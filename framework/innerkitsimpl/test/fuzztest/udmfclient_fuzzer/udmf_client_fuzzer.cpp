0cfa57a9eb54155741adbe504593e127f806725c
zph <zhaopenghui8@huawei.com>
2025-03-07 18:55:38 +0800

    update
    Signed-off-by: zph <zhaopenghui8@huawei.com>

--- framework/innerkitsimpl/test/fuzztest/udmfclient_fuzzer/udmf_client_fuzzer.cpp
+++ framework/innerkitsimpl/test/fuzztest/udmfclient_fuzzer/udmf_client_fuzzer.cpp
@@ -86,6 +86,7 @@ void SetUpTestCase()
 
 void TearDown()
 {
+    std::this_thread::sleep_for(std::chrono::seconds(3));
 }
 
 void SetNativeToken()
@@ -622,7 +623,6 @@ void StartAsyncDataRetrievalFuzz(const uint8_t *data, size_t size)
     params.progressIndicator = ProgressIndicator::DEFAULT;
     params.progressListener = [](ProgressInfo progressInfo, std::shared_ptr<UnifiedData> data) {};
     UdmfAsyncClient::GetInstance().StartAsyncDataRetrieval(params);
-    std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 void CancelAsyncDataRetrievalFuzz(const uint8_t *data, size_t size)
@@ -644,7 +644,6 @@ void CancelAsyncDataRetrievalFuzz(const uint8_t *data, size_t size)
     };
     UdmfAsyncClient::GetInstance().StartAsyncDataRetrieval(params);
     UdmfAsyncClient::GetInstance().Cancel(key);
-    std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 }
 
