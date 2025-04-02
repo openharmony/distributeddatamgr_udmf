2283ef09875f1e88e3d075b35fc1e8d624047c53
zhangqing <zhangqing50@h-partners.com>
2025-04-02 16:02:48 +0800

    arkui-x update master OH to weekly_20250331 and OH fix build bug
    Signed-off-by: zhangqing <zhangqing50@h-partners.com>
    Change-Id: Ib444f0dbf439402965a79589e0a03cc70e2b087e

--- framework/innerkitsimpl/data/unified_data.cpp
+++ framework/innerkitsimpl/data/unified_data.cpp
@@ -30,7 +30,9 @@ UnifiedData::UnifiedData()
     properties_ = std::make_shared<UnifiedDataProperties>();
     auto duration = std::chrono::system_clock::now().time_since_epoch();
     properties_->timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+#ifndef CROSS_PLATFORM
     sdkVersion_ = UTILS::GetCurrentSdkVersion();
+#endif
 }
 
 UnifiedData::UnifiedData(std::shared_ptr<UnifiedDataProperties> properties)
@@ -42,7 +44,9 @@ UnifiedData::UnifiedData(std::shared_ptr<UnifiedDataProperties> properties)
     properties_ = properties;
     auto duration = std::chrono::system_clock::now().time_since_epoch();
     properties_->timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+#ifndef CROSS_PLATFORM
     sdkVersion_ = UTILS::GetCurrentSdkVersion();
+#endif
 }
 
 int64_t UnifiedData::GetSize()
