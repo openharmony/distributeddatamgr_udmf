bf0c96f4af90245a341ea67963ff3c014c7cdc65
wenjinchao <wenjinchao@huawei.com>
2025-04-07 16:58:22 +0800

    fix problem
    Signed-off-by: wenjinchao <wenjinchao@huawei.com>
    Change-Id: Ic10a37c4948341136df4ed1aacb2bfb853259029

--- framework/innerkitsimpl/data/unified_data.cpp
+++ framework/innerkitsimpl/data/unified_data.cpp
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "unified_meta.h"
 #define LOG_TAG "UnifiedData"
 #include "logger.h"
 #include "unified_data.h"
