be2628162d0c46c1455bc3570041c31a99065ac5
wenjinchao <wenjinchao@huawei.com>
2025-04-07 16:57:16 +0800

    fix problem
    Signed-off-by: wenjinchao <wenjinchao@huawei.com>
    Change-Id: I80696410816460fad2d2a8db6cdc90a0e62572c9

--- framework/innerkitsimpl/data/unified_data_helper.cpp
+++ framework/innerkitsimpl/data/unified_data_helper.cpp
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define LOG_TAG "UnifiedDataHelper"
 #include "unified_data_helper.h"
 
 #include "common_func.h"
