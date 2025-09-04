e576790ceeb7ab5a4752784c0c84a5611cc2bf67
wenjinchao <wenjinchao@huawei.com>
2025-09-04 14:09:23 +0800

    fix js tdd:
    Signed-off-by: wenjinchao <wenjinchao@huawei.com>
    Change-Id: I8f5cdcc5b2688452ef48fdd2106d34d76563b674

--- framework/jskitsimpl/unittest/UdmfCallbackJsTest.js
+++ framework/jskitsimpl/unittest/UdmfCallbackJsTest.js
@@ -237,6 +237,46 @@ describe('UdmfCallbackJSTest', function () {
   });
 
   /**
+   * @tc.name UdmfQueryCallbackSucTest
+   * @tc.desc Test Js Api queryData successful
+   * @tc.type: FUNC
+   * @tc.require: issueNumber
+   */
+  it('UdmfQueryCallbackSucTest', 0, async function (done) {
+    const TAG = 'UdmfQueryCallbackSucTest:';
+    console.info(TAG, 'start');
+    try {
+      UDC.deleteData(optionsValid, (err, data) => {
+        expect(err).assertUndefined();
+        console.info(TAG, 'delete success.');
+        UDC.queryData(optionsValid, (err, data) => {
+          console.info(TAG, 'query has no data.');
+          expect(err).assertUndefined();
+          UDC.insertData(optionsValid, unifiedData01, (err, data) => {
+            expect(err).assertUndefined();
+            console.info(TAG, `insert success. The key: ${data}`);
+            UDC.insertData(optionsValid, unifiedData02, (err, data) => {
+              expect(err).assertUndefined();
+              console.info(TAG, `insert success. The key: ${data}`);
+              UDC.queryData(optionsValid, function (err, data) {
+                expect(err).assertUndefined();
+                console.info(TAG, 'query success.data.length=' + data.length);
+                expect(data.length >= 2).assertTrue();
+                done();
+              });
+            });
+          });
+        });
+      });
+    } catch (e) {
+      console.error(TAG, 'Unreachable code!');
+      expect(null).assertFail();
+      done();
+    }
+    console.info(TAG, 'end');
+  });
+
+  /**
    * @tc.name UdmfDeleteCallbackInvalidOptionsTest
    * @tc.desc Test Js Api deleteData with invalid options
    * @tc.type: FUNC
